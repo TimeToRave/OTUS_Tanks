e349345ba278b34cb24e9d82f2accb7ef6191df1
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-02-22 21:34:37 +0300

    Реализована команда Move

---
+++ Tanks/Classes/Commands/Move.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tanks.Interfaces;
+using Tanks.Classes;
+
+namespace Tanks.Classes.Commands
+{
+	class Move : ICommand 
+	{
+		IMovable MovableEntity { get; set; }
+
+		public Move(IMovable movableEntity)
+		{
+			MovableEntity = movableEntity;
+		}
+
+		public bool Execute ()
+		{
+			MovableEntity.Position = MovableEntity.Position + MovableEntity.Velocity;
+			return true;
+		}
+	}
+}
