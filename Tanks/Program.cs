21ff3f9cba60ff7a601ab87098164e47a2768787
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-02-22 21:37:53 +0300

    Основной цикл программы (на данный момент)

---
+++ Tanks/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Tanks.Interfaces;
+using Tanks.Classes;
+using Tanks.Classes.Objects;
+
+namespace Tanks
+{
+	class Program
+	{
+		static void Main()
+		{
+			List<IEntity> gameObjects = new List<IEntity>();
+
+			Tank tank = new Tank(
+				new Point(0, 0),
+				new Point(1, 1),
+				new List<string>
+				{
+					"Move",
+					"Move",
+					"Move"
+				}
+			);
+
+			gameObjects.Add(tank);
+
+			for (int i = 0; i < 100; i++)
+			{
+				Update(i, gameObjects);
+			}
+
+			Console.ReadKey();
+		}
+
+		public static void Update(int tick, List<IEntity> gameObjects)
+		{
+			foreach (IEntity gameObject in gameObjects)
+			{
+				int index = tick % gameObject.Behavior.Count;
+				gameObject.Behavior[index].Execute();
+				Console.WriteLine(gameObject.GetInfo());
+			}
+		}
+	}
+
+
+}
