68cd2f762ff902e403711e945c5bf3cbbb293785
Nick Kuzmin <nickkuzmin1301@gmail.com>
2021-02-22 21:34:22 +0300

    Добавлен адаптер для интерфейса IMovable

---
+++ Tanks/Adapters/MovableAdapter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tanks.Classes;
+using Tanks.Interfaces; 
+
+namespace Tanks.Adapters
+{
+	class MovableAdapter : IMovable
+	{
+		IEntity Entity { get; set; }
+		
+		public MovableAdapter(IEntity entity)
+		{
+			Entity = entity;
+		}
+
+		public Point Position
+		{
+			get
+			{
+				return (Point)Entity["Position"];
+			}
+			set
+			{
+				Entity["Position"] = value;
+			}
+		}
+
+		public Point Velocity
+		{
+			get
+			{
+				return (Point)Entity["Velocity"];
+			}
+			set
+			{
+				Entity["Velocity"] = value;
+			}
+		}
+	}
+}
